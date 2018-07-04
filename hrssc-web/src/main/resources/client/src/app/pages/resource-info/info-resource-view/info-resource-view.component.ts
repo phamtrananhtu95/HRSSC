42ab73c6a45cc28a4e8542ae33de442ca160db79
AnhTu <phamtrananhtu95@yahoo.com.vn>
2018-07-04 16:44:15 +0700

    push tam
    Change-Id: I79a4b3a074e38ed9e5117b182bc9fc43a94f02f0

--- hrssc-web/src/main/resources/client/src/app/pages/resource-info/info-resource-view/info-resource-view.component.ts
+++ hrssc-web/src/main/resources/client/src/app/pages/resource-info/info-resource-view/info-resource-view.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { Employee, Skill } from './../../../models/employee.model';
+
 import * as jQuery from 'jquery';
+import { EmployeeService } from '../../../services/employee.service';
 declare var $: any;
 
 @Component({
@@ -8,10 +13,52 @@ declare var $: any;
 })
 export class InfoResourceViewComponent implements OnInit {
 
+
+    @Input() humanResource: Employee;
+
+    public skillList: string;
+
     constructor(
+        private router: Router,
+        private route: ActivatedRoute,
+        private employeeService: EmployeeService,
+
     ) { }
 
-    ngOnInit() {
+    ngOnChanges() {
+        this.getHumanResourceById();
+
     }
 
+    ngOnInit() {
+
+    }
+
+    getHumanResourceById() {
+        let humanResourceId = this.route.snapshot.queryParams['id'];
+        this.skillList = "";
+        this.employeeService.getHumanResourceById(humanResourceId).subscribe(
+            res => {
+                // this.avaliableDate = this.ConvertToDatetime(this.humanResource.availableDate);
+
+                this.humanResource = res;
+                // console.log(this.isOwnManager);
+                console.log(this.humanResource.availableDate);
+
+                // console.log("----------" + JSON.stringify(this.humanResource));
+                let skills = this.humanResource.resourceSkillsById;
+                if (!skills || skills.length < 1) {
+                    return;
+                }
+                for (let i = 0; i < skills.length - 1; i++) {
+                    this.skillList = this.skillList + skills[i].skillBySkillId.title + ", ";
+                }
+                this.skillList = this.skillList + skills[skills.length - 1].skillBySkillId.title;
+                // console.log("iiiiiiiii" + this.userByUserId);
+            },
+            err => {
+
+            }
+        )
+    }
 }
